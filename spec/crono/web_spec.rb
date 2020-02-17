a9a51e98bb0f812f50c9218e3c96d47fbb86f613
Nicolas Rodriguez <nicoladmin@free.fr>
2020-02-17 21:01:14 +0100

    Test Crono as a mounted engine

--- spec/crono/web_spec.rb
+++ spec/crono/web_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Crono::Web do
 
-  let(:app) { Crono::Web }
+  let(:app) { Rails.application }
 
   include Rack::Test::Methods
 
@@ -20,33 +20,33 @@ RSpec.describe Crono::Web do
 
   describe '/' do
     it 'should show all jobs' do
-      get '/'
+      get '/crono'
       expect(last_response).to be_ok
       expect(last_response.body).to include @test_job_id
     end
 
     it 'should show a error mark when a job is unhealthy' do
       @test_job.update(healthy: false, last_performed_at: 10.minutes.ago)
-      get '/'
+      get '/crono'
       expect(last_response.body).to include 'Error'
     end
 
     it 'should show a success mark when a job is healthy' do
       @test_job.update(healthy: true, last_performed_at: 10.minutes.ago)
-      get '/'
+      get '/crono'
       expect(last_response.body).to include 'Success'
     end
 
     it 'should show a pending mark when a job is pending' do
       @test_job.update(healthy: nil)
-      get '/'
+      get '/crono'
       expect(last_response.body).to include 'Pending'
     end
   end
 
   describe '/job/:id' do
     it 'should show job log' do
-      get "/job/#{@test_job.id}"
+      get "/crono/job/#{@test_job.id}"
       expect(last_response).to be_ok
       expect(last_response.body).to include @test_job_id
       expect(last_response.body).to include @test_job_log
@@ -55,7 +55,7 @@ RSpec.describe Crono::Web do
     it 'should show a message about the unhealthy job' do
       message = 'An error occurs during the last execution of this job'
       @test_job.update(healthy: false)
-      get "/job/#{@test_job.id}"
+      get "/crono/job/#{@test_job.id}"
       expect(last_response.body).to include message
     end
   end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,2 +1,3 @@
 Rails.application.routes.draw do
+  mount Crono::Web, at: '/crono'
 end
