9ae7a8647dac7b38b01128cd98d573f5747e2451
Nicolas Rodriguez <nico@nicoladmin.fr>
2022-03-02 03:01:01 +0100

    Do Sprockets configuration in an initializer

--- lib/crono/engine.rb
+++ lib/crono/engine.rb
@@ -5,7 +5,14 @@ module Crono
     isolate_namespace Crono
 
     initializer 'crono.assets.precompile' do |app|
-      app.config.assets.precompile += %w( crono/application.css crono/materialize.min.css )
+      if app.config.respond_to?(:assets)
+        if defined?(Sprockets) && Sprockets::VERSION >= "4"
+          app.config.assets.precompile += %w( crono/application.css crono/materialize.min.css )
+        else
+          app.config.assets.precompile << proc { |path| path == 'crono/application.css' }
+          app.config.assets.precompile << proc { |path| path == 'crono/materialize.min.css' }
+        end
+      end
     end
 
   end
