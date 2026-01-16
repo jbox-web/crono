583418da02914ae7a5e4db240fc06da229e6b936
Nicolas Rodriguez <nico@nicoladmin.fr>
2026-01-17 00:22:18 +0100

    Enable random spec

--- spec/config_rspec.rb
+++ spec/config_rspec.rb
@@ -5,8 +5,8 @@ RSpec.configure do |config|
   # Use DB agnostic schema by default
   load Rails.root.join('db', 'schema.rb').to_s
 
-  # config.order = :random
-  # Kernel.srand config.seed
+  config.order = :random
+  Kernel.srand config.seed
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
